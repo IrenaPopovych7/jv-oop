2209c555474c9369c38b5a636da2bc183e43331d
iryna <popovicirina40@gmail.com>
2025-12-07 11:30:39 +0000

    add solution

---
+++ src/main/java/core/basesyntax/Excavator.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Excavator extends Machine{
+        @Override
+        public void doWork() {
+            System.out.println("started its work");
+        };
+
+        @Override
+        public void stopWork() {
+            System.out.println("stopped working");
+        }
+    }
+
+
